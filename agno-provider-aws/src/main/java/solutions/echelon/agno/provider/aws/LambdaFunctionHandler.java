decba6f4087602ade9712936a05d5ae35d01c5e8
Khalid Zoabi <kzoabi@outlook.com>
2017-04-27 08:13:59 -0400

    Adding the agno provider for AWS that provides a standard interface for connecting a FAAS to an AWS Lambda function handler provider.

---
+++ agno-provider-aws/src/main/java/solutions/echelon/agno/provider/aws/LambdaFunctionHandler.java
@@ -0,0 +1,23 @@
+package solutions.echelon.agno.provider.aws;
+
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import solutions.echelon.agno.common.provider.FunctionProvider;
+
+public interface LambdaFunctionHandler<FunctionRequestType, FunctionResponseType>
+    extends
+        RequestHandler<FunctionRequestType, LambdaFunctionAPIGatewayResponse>,
+        FunctionProvider<FunctionRequestType, LambdaFunctionAPIGatewayResponse, FunctionRequestType, FunctionResponseType>
+{    
+    @Override
+    public default void deploy()
+    {
+        // TODO deploy the function to Lambda
+    }
+    
+    @Override
+    public default void undeploy()
+    {
+        // TODO undeploy the function from Lambda
+    }
+}
